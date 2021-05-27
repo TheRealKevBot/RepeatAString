5e45237ef2e68cd7692641ad48839d5572e51822
TheRealKevBot <Kglas001@gmail.com>
2021-05-26 19:13:23 -0600

    Build

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 // Repeat a given string str (first argument) for num times (second argument). 
-// Return an empty string if num is not a positive number. 
+// Return no can do if num is not a positive number. 
+
+// function repeatStringNumTimes(str, num) {
+//     if (num >= 0) {
+//         return str.repeat(num)
+//     } else {
+//         return "No can do"
+//     }
+// }
+
 // For the purpose of this challenge, do not use the built-in .repeat() method. 
 
 function repeatStringNumTimes(str, num) {
-    return str;
-  }
+    if (num >= 0) {
+        let count = 1
+        let numArray = []
+        while (count <= num) {
+            numArray.push(str)
+            count ++
+        }
+        str = numArray.join('')
+        return str
+    } else {
+        return "No can do"
+    }
+}
   
-  repeatStringNumTimes("abc", 3);
+  console.log(repeatStringNumTimes("abc", 3));
+  console.log(repeatStringNumTimes("abc", -3));
+  console.log(repeatStringNumTimes("abc", 4));
+  console.log(repeatStringNumTimes("abc", 1));
+  console.log(repeatStringNumTimes("*", 8));
+  console.log(repeatStringNumTimes("abc", 200));
+  console.log(repeatStringNumTimes("abc", 50));
+  console.log(repeatStringNumTimes("abc", 489));
+  console.log(repeatStringNumTimes("abc", 1000));
+  console.log(repeatStringNumTimes("abc", 360));
